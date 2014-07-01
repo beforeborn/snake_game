4ead385938533b3580263784524e395af3f8ff4a
beforeborn <sjz46860404@126.com>
2014-07-01 18:21:46 +0800

    222

--- Classes/MainMenu.cpp
+++ Classes/MainMenu.cpp
@@ -1,5 +1,6 @@
 #include "MainMenu.h"
-
+#include "Game.h"
+#include "Help.h"
 
 MainMenu::MainMenu(void)
 {
@@ -26,22 +27,45 @@ bool MainMenu::init()
 	}
 	//添加背景
 	auto size=Director::getInstance()->getWinSize();
-	auto spriteBK=Sprite::create("");
+	auto spriteBK=Sprite::create("menu_bg.jpg");
 	spriteBK->setPosition(Point(size.width/2,size.height/2));
 	this->addChild(spriteBK);
 	//添加2个菜单条目
 
 	auto menuItemStart=MenuItemFont::create("Start",CC_CALLBACK_1(MainMenu::menuCallBack,this));
 	auto menuItemHelp=MenuItemFont::create("Help",CC_CALLBACK_1(MainMenu::menuCallBack,this));
+	menuItemStart->setTag(1);
+	menuItemHelp->setTag(2);
+
 
 	auto menu=Menu::create(menuItemStart,menuItemHelp,NULL);
-	menu->setAnchorPoint(Point::ZERO);
-	menu->setPosition(Point(size.width-menuItemStart->getContentSize().width-10,size.height-menuItemStart->getContentSize().height-70));
-	menu->setPosition(Point(size.width-menuItemHelp->getContentSize().width-10,size.height-menuItemHelp->getContentSize().height-30));
+	menu->setPosition(Point::ZERO);
+	menuItemStart->setPosition(Point(size.width-menuItemStart->getContentSize().width-12,menuItemStart->getContentSize().height+70));
+	menuItemHelp->setPosition(Point(size.width-menuItemHelp->getContentSize().width-25,menuItemHelp->getContentSize().height+30));
 
+	menuItemStart->setColor(Color3B::BLACK);
+	menuItemHelp->setColor(Color3B::BLACK);
+
+	//menuItemStart->setFontSize(40);
+
+	this->addChild(menu);
 	return true;
 }
-void menuCallBack(Ref * obj)
+void MainMenu::menuCallBack(Ref * obj)
 {
+	Node* node=(Node*)obj;
+	Scene * scene=NULL;
+	switch (node->getTag())
+	{
+	case 1:
+		scene=Game::createScene();
+		break;
+	case 2:
+		scene=Help::createScene();
+		break;
+	default:
+		break;
+	}
+	Director::getInstance()->pushScene(scene);
 
 }
